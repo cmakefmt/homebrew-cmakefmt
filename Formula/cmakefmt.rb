9ce28828aa7c859041d23204ddaadb9f28e0ce3a
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2026-06-22 13:03:57 +0100

    ci: commit generated homebrew bottle block

--- Formula/cmakefmt.rb
+++ Formula/cmakefmt.rb
@@ -9,6 +9,14 @@ class Cmakefmt < Formula
   sha256 "80b2880b5774779e974f941752111c73a147bb8fa94d2117ddeb91aa716eae29"
   license any_of: ["MIT", "Apache-2.0"]
 
+  bottle do
+    root_url "https://github.com/cmakefmt/homebrew-cmakefmt/releases/download/v1.7.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "28cd7c64eba44e87f1f3621acbd1f798a11143089202290b8f4e7a25d44c2696"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "063bebb7568d236cc820639234472890ce4fdd3de3d9227a6941daf0ac3262a3"
+    sha256 cellar: :any_skip_relocation, tahoe:         "eaf162b3e19a75bc7ae777dbf112b7771dad1e2bb822b57fbe4f67721094a3f8"
+    sha256 cellar: :any_skip_relocation, sequoia:       "da2623852eb7ea6ac082d6849dad46cfde43abbfdfb3a99526cfba40fe93c3b9"
+  end
+
   depends_on "rust" => :build
 
   def install
